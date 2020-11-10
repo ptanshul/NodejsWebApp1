83c2642a08f358b9fd0fae44715cd38ba271d634
Gustavo Lopez <gulopez@microsoft.com>
2020-11-10 12:05:16 -0800

    update 4

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n' + process.env.myMongoDB  );
+    res.end('Hello World \n' + process.env.myMongoDB  );
 }).listen(port);
